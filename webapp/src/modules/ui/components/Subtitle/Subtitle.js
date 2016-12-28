241bd453b8a46caee14acdd5e848569d2b101ffe
yesmagaly <yesmagaly@gmail.com>
2016-12-28 16:29:30 -0500

    Moved components to ui module.

--- webapp/src/modules/ui/components/Subtitle/Subtitle.js
+++ webapp/src/modules/ui/components/Subtitle/Subtitle.js
@@ -1,13 +1,14 @@
 import React, {PropTypes} from 'react'
+import classnames from 'classnames'
 
 import styles from './Subtitle.css'
 
 export default class Subtitle extends React.Component {
   render() {
-    const { children } = this.props
+    const { children, className} = this.props
 
     return (
-      <div className={ styles.subtitle}>{ children }</div>
+      <div className={ classnames(styles.subtitle, className) }>{ children }</div>
     )
   }
 }
